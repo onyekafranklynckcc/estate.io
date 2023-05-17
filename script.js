c331d81647533518fdc87de76032a72aa7e93b4b
franklynnaza <119573751+franklynnaza@users.noreply.github.com>
2023-05-17 07:45:56 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -76,29 +76,6 @@ function loginn(){
     indicatorr.style.transform = "translateX(00px)"
 };
 
-var firstIndex=0;
-
-function automaticSlide(){
-
-setTimeout(automaticSlide, 2500); var pics;
-
-const img=document.querySelectorAll('.blog-img');
-
-for(pics=0; pics<img.length;pics++){ img[pics].style.display="none";
-
-}
-
-firstIndex++;
-
-if(firstIndex > img.length) { firstIndex =1;
-
-}
-
-img[firstIndex -1].style.display="block";
-
-} automaticSlide();
-var right = document.getElementsByClassName(".a-rght")
-
 
 
 
