f1236f51af0f9f08910fe3d635a2c1f6a0cee6da
franklynnaza <119573751+franklynnaza@users.noreply.github.com>
2023-06-02 18:00:16 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+function handlePreloader() {
+  if($('.loader-wrap').length){
+      $('.loader-wrap').delay(1000).fadeOut(500);
+  }
+  TweenMax.to($(".loader-wrap .overlay"), 1.2, {
+      force3D: true,
+      left: "100%",
+      ease: Expo.easeInOut,
+  });
+}
+
+if ($(".preloader-close").length) {
+  $(".preloader-close").on("click", function(){
+      $('.loader-wrap').delay(200).fadeOut(500);
+  })
+}
+
+$(window).on('load', function() {
+  handlePreloader();
+  enableMasonry();
+});
+
 
 
 var faf = document.querySelector(".navbar-toggler");
@@ -113,3 +135,7 @@ function next(){
     function nextImage() {
       showImage(currentImageIndex + 1);
     }
+
+
+    
+    
